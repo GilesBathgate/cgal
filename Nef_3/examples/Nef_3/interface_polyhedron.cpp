93ff95394cfb29ccabb65604706594b2eabf05ec
Giles Bathgate <giles.bathgate@gmail.com>
2017-11-20 20:46:01 +0000

    Performance test convert_to_polyhedron (original)

--- Nef_3/examples/Nef_3/interface_polyhedron.cpp
+++ Nef_3/examples/Nef_3/interface_polyhedron.cpp
@@ -17,14 +17,10 @@ int main() {
   std::cin >> P;
   if(P.is_closed()) {
     Nef_polyhedron N1(P);
-    Nef_polyhedron N2(N1);
-    Aff_transformation_3 aff(CGAL::TRANSLATION, Vector_3(2,2,0,1));
-    N2.transform(aff);
-    N1 += N2;
 
     if(N1.is_simple()) {
-      N1.convert_to_polyhedron(P);
-      std::cout << P;
+      for(int i=0; i<1000; ++i)
+        N1.convert_to_polyhedron(P);
     }
     else
       std::cerr << "N1 is not a 2-manifold." << std::endl;
