7fdb721cbf806a2909629e505368d1eda7b1c864
Giles Bathgate <giles.bathgate@gmail.com>
2017-12-14 20:36:36 +0000

    Restore stl output

--- Nef_3/examples/Nef_3/interface_polyhedron.cpp
+++ Nef_3/examples/Nef_3/interface_polyhedron.cpp
@@ -21,6 +21,47 @@ typedef Kernel::FT Scalar;
 typedef Polyhedron::Facet_const_iterator FacetIterator;
 typedef Polyhedron::Halfedge_around_facet_const_circulator HalffacetCirculator;
 
+void exportAsciiSTL(Polyhedron poly, std::ostream& output)
+{
+
+	output << "solid stl\n";
+
+	for(FacetIterator fi=poly.facets_begin(); fi!=poly.facets_end(); ++fi) {
+		HalffacetCirculator hc=fi->facet_begin();
+		//CGAL_assertion(circulator_size(hc)==3);
+		Triangle_3 t((hc++)->vertex()->point(),
+					 (hc++)->vertex()->point(),
+					 (hc++)->vertex()->point());
+
+		Vector_3 n=t.supporting_plane().orthogonal_vector();
+		Scalar ls=n.squared_length();
+		Scalar l=Scalar(sqrt(CGAL::to_double(ls)));
+		Vector_3 un=n/l;
+
+		float x1=CGAL::to_double(t[0].x());
+		float y1=CGAL::to_double(t[0].y());
+		float z1=CGAL::to_double(t[0].z());
+		float x2=CGAL::to_double(t[2].x());
+		float y2=CGAL::to_double(t[2].y());
+		float z2=CGAL::to_double(t[2].z());
+		float x3=CGAL::to_double(t[1].x());
+		float y3=CGAL::to_double(t[1].y());
+		float z3=CGAL::to_double(t[1].z());
+		float nx=CGAL::to_double(un.x());
+		float ny=CGAL::to_double(un.y());
+		float nz=CGAL::to_double(un.z());
+
+		output << "  facet normal " << nx << " " << ny << " " << nz << "\n";
+		output << "    outer loop\n";
+		output << "      vertex " << x1 << " " << y1 << " " << z1 << "\n";
+		output << "      vertex " << x2 << " " << y2 << " " << z2 << "\n";
+		output << "      vertex " << x3 << " " << y3 << " " << z3 << "\n";
+		output << "    endloop\n";
+		output << "  endfacet\n";
+	}
+	output << "endsolid stl\n";
+}
+
 int main(int argc, char** argv)
 {
 	int N=10000;
@@ -47,6 +88,13 @@ int main(int argc, char** argv)
 	timer.stop();
 	std::cout << "convert_nef_polyhedron_to_polygon_mesh " << timer.time() << "\n";
 
+	{
+		Polyhedron poly_out;
+		CGAL::convert_nef_polyhedron_to_polygon_mesh(nef, poly_out);
+		std::ofstream output("cube1.stl");
+		exportAsciiSTL(poly_out,output);
+		output.close();
+	}
 
 	timer.reset();
 	timer.start();
@@ -60,6 +108,14 @@ int main(int argc, char** argv)
 	timer.stop();
 	std::cout << "convert_nef_polyhedron_to_polygon_mesh + triangulate_faces " << timer.time() << "\n";
 
+	{
+		Polyhedron poly_out;
+		CGAL::convert_nef_polyhedron_to_polygon_mesh(nef, poly_out);
+		CGAL::Polygon_mesh_processing::triangulate_faces(poly_out);
+		std::ofstream output("cube2.stl");
+		exportAsciiSTL(poly_out,output);
+		output.close();
+	}
 
 	timer.reset();
 	timer.start();
@@ -71,6 +127,15 @@ int main(int argc, char** argv)
 	}
 	timer.stop();
 	std::cout << "convert_nef_polyhedron_to_polygon_mesh(true) " << timer.time() << "\n";
+
+	{
+		Polyhedron poly_out;
+		CGAL::convert_nef_polyhedron_to_polygon_mesh(nef, poly_out,true);
+		std::ofstream output("cube3.stl");
+		exportAsciiSTL(poly_out,output);
+		output.close();
+	}
+
 
 
 	timer.reset();
@@ -84,6 +149,13 @@ int main(int argc, char** argv)
 	timer.stop();
 	std::cout << "convert_to_polyhedron " << timer.time() << "\n";
 
+	{
+		Polyhedron poly_out;
+		nef.convert_to_polyhedron(poly_out);
+		std::ofstream output("cube4.stl");
+		exportAsciiSTL(poly_out,output);
+		output.close();
+	}
 
 	return 0;
 
