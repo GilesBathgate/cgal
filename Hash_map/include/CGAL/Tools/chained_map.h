aa17930c8ae36b6fbc913c702ee312ef7701ae8f
Giles Bathgate <giles.bathgate@gmail.com>
2021-04-25 16:16:09 +0100

    Use minimum size of 32

--- Hash_map/include/CGAL/Tools/chained_map.h
+++ Hash_map/include/CGAL/Tools/chained_map.h
@@ -83,6 +83,7 @@ private:
    }
 
 public:
+   static constexpr std::size_t min_size = 32;
    typedef chained_map_elem<T>*  chained_map_item;
    typedef chained_map_item item;
 
@@ -273,8 +274,8 @@ template <typename T, typename Allocator>
 chained_map<T, Allocator>::chained_map(std::size_t n) :
   nullptrKEY(0), NONnullptrKEY(1), old_table(0)
 {
-  if (n < 512)
-    init_table(512);
+  if (n < min_size)
+    init_table(min_size);
   else {
     std::size_t ts = 1;
     while (ts < n) ts <<= 1;
@@ -336,7 +337,7 @@ void chained_map<T, Allocator>::clear()
     destroy(item);
   alloc.deallocate(table, table_end - table);
 
-  init_table(512);
+  init_table(min_size);
 }
 
 template <typename T, typename Allocator>
